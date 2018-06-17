ecd299142fdd5de0cb3105f61b0280b9e7cea114
Walt Panfil <panfilwk@rose-hulman.edu>
2018-06-17 19:42:27 -0400

    ReST-ful API support added on server side

--- http/server.py
+++ http/server.py
@@ -5,12 +5,30 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('index.html')
 
-def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
+class ReSTHandler(tornado.web.RequestHandler):
+    pressCount = {}
+
+    def initialize(self, pressCount):
+        self.pressCount = pressCount
+
+    def get(self):
+        self.write(self.pressCount)
+    
+    def post(self):
+        self.pressCount['count'] += 1
+
+class Server:
+
+    pressCount = {'count' : 0}
+
+    def start_app(self):
+        app = tornado.web.Application([
+            (r"/", MainHandler),
+            (r"/count", ReSTHandler, {'pressCount': self.pressCount})
+        ])
+        app.listen(8888)
+        tornado.ioloop.IOLoop.current().start()
 
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+    server = Server()
+    server.start_app()
